e16447c629dde5f95d369ab8f6c5543a361ecd3d
Walter P. Little <wplittle@users.noreply.github.com>
2018-02-08 20:42:28 -0800

    api routers initial setup

---
+++ api/employees.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const employeesRouter = express.Router();
+
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+const timesheetsRouter = require('./timesheets');
+
+module.exports = employeesRouter;
--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const errorhandler = require('errorhandler');
 
+const apiRouter = require('./api/api');
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(errorhandler());
+
+app.use('/api', apiRouter);
 
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
